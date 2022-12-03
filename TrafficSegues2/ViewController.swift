a925fabd8a41d8d452ed78c42e3c651a13de7391
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2022-12-03 14:43:30 -0500

    conditional segue

--- TrafficSegues2/ViewController.swift
+++ TrafficSegues2/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueShouldOccur: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goToGreen(_ sender: Any) {
+        if segueShouldOccur.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+    @IBAction func goToYellow(_ sender: Any) {
+        if segueShouldOccur.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
 }
 
