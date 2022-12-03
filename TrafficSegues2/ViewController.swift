ea5528003259711f1889e2a44c62a700a287010c
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2022-12-03 14:24:15 -0500

    Initial Commit

---
+++ TrafficSegues2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficSegues2
+//
+//  Created by Matthew Soto on 12/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
